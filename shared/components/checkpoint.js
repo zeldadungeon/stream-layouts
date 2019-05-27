2d08606887308c4542d8c7ca9ac90602b6164f1a
LockeExile <lockeexile@gmail.com>
2019-05-27 12:50:41 -0700

    update elimination checkpoint graphic

--- shared/components/checkpoint.js
+++ shared/components/checkpoint.js
@@ -4,48 +4,46 @@
     Vue.component("zd-checkpoint", {
         template: `<div class="zd-checkpoint" style="text-align: center; padding: 10px 15px; box-sizing: border-box;">
             <div style="display: inline-block; text-align: left;">
-                <div v-if="num < checkpoints.length">Checkpoint {{ num + 1 }}: {{ count }} / {{ target }}<br />{{ checkpoints[num] }}<br /></div>
+                <div v-if="checkpoint < checkpoints.length">Checkpoint {{ checkpoint + 1 }}: {{ count }} / {{ target }}<br />{{ checkpoints[checkpoint] }}<br /></div>
                 <div style="display: flex; flex-wrap: wrap;">
                     <div v-for="finish in finished" style="flex: 1 1 50%; white-space: nowrap;">{{ finish }}</div>
                 </div>
             </div>
         </div>`,
-        replicants: ["players", "stopwatch"],
+        props: ["runName"],
+        replicants: ["runs"],
         data: function() {
             return {
                 checkpoints: [
-                    "Enter Deepwood Shrine",
-                    "Get Gust Jar",
-                    "Finish Deepwood Shrine",
-                    "Enter Cave of Flames",
-                    "Get Cane of Pacci",
-                    "Finish Cave of Flames",
-                    "Get Pegasus Boots",
-                    "Enter Fortress of Winds"
+                    "Deepwood Shrine",
+                    "Gust Jar",
+                    "Earth Element",
+                    "Cave of Flames",
+                    "Cane of Pacci",
+                    "Fire Element",
+                    "Pegasus Boots",
+                    "Fortress of Winds"
                 ]
             };
         },
         computed: {
-            num: function() {
-                const worstPlace = this.checkpoints.length + 3;
-                let bestPlace = Object.keys(this.players).reduce((acc, cur) => Math.min(acc, this.players[cur].place || worstPlace), worstPlace);
-                return this.checkpoints.length + 3 - bestPlace;
+			run() {
+				return this.runs && this.runs.start && this.runs[this.runName || this.runs.start.current] || { racers: [] };
             },
-            target: function() {
-                return this.checkpoints.length - this.num + 1;
+            checkpoint() {
+                return this.run.racers.filter(r => r.state === "eliminated").length;
             },
-            count: function() {
-                return Object.keys(this.players).filter(p => this.players[p].checkpoints && this.players[p].checkpoints[this.checkpoints[this.num]]).length;
+            target() {
+                return this.checkpoints.length - this.checkpoint + 1;
             },
-            finished: function() {
-                return !this.stopwatch.results ? [] : this.stopwatch.results
-                    .filter(p => this.players[p].place)
-                    .sort((a, b) => this.players[a].place - this.players[b].place)
-                    .map(p => {
-                        const position = this.players[p].place;
-
-                        return `${position}${position === 1 ? "st" : position === 2 ? "nd" : position === 3 ? "rd" : "th"}: ${p}`
-                    });
+            count() {
+                return this.run.racers.filter(r => r.checkpoint > this.checkpoint).length;
+            },
+            finished() {
+                return this.run.racers
+                    .filter(r => r.state === "eliminated")
+                    .sort((a, b) => a.position - b.position)
+                    .map(r => `${r.position}${{1:"st",2:"nd",3:"rd"}[r.position]||"th"}: ${r.name}`);
             }
         }
     });
